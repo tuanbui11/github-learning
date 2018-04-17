d3f1ed9e739e173b31801318aa21188cab70bc78
Tuan Bui <bui.quoctuan@outlook.com>
2018-04-17 15:13:21 +0700

    Commit Nodejs

---
+++ helloworld.js
@@ -0,0 +1,8 @@
+var http = require("http");
+http.createServer(function (request, response)
+{
+	response.writeHead(200, {'Content-Type': 'Text/plain'});
+	response.end('Hello World\n');
+}).listen(8081);
+
+console.log('Server');
